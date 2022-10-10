05717e07d3b27156c5606efe2c85f33c6fb423f0
j2sleek <j2sleek@gmail.com>
2022-10-10 17:13:25 +0100

    added landing component

---
+++ src/Landing.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from './logo.svg';
+
+function Landing() {
+    return (
+        <div>
+            <nav>
+                <img src={logo} alt="logo" />
+            </nav>
+            <div></div>
+            <div>
+                <form>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
